fc1640e73d83f0611b57df00ff7cd192a7251977
MatthewR2D2 <matthew.c.r.millar@gmail.com>
2020-05-25 13:57:54 +0900

    Configuration file

---
+++ Config.py
@@ -0,0 +1,14 @@
+
+# Openweather information
+BASE_URL = "http://api.openweathermap.org/data/2.5/weather?"
+API_KEY = '****************************' #Change to correct API_KEY from openweatherAPI
+
+# GIS infomarion
+'''
+# NOTE Ensure to have all related files in the same folder as the shp (shapefile) 
+There will be errors if you do not
+'''
+WORLD_SHAPEFILE = "Data/MapShapeFiles/World_Countries.shp"
+
+CITY_LIST = "../Data/LargeCityList.csv"
+MAJOR_CITIES_INFO = "Data/MajorCityInfo.csv"
